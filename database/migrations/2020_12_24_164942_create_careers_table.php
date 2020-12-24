f598ac2dbcde675205d6a3f858d9bc3724587316
BernaGN <31524330+BernaGN@users.noreply.github.com>
2020-12-24 17:07:54 -0600

    Creacion de migraciones yconfiguracion horaria, lenguaje español para los errores

---
+++ database/migrations/2020_12_24_164942_create_careers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCareersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('careers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code', 10);
+            $table->string('name', 100);
+            $table->integer('faculty_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('careers');
+    }
+}
---
+++ database/migrations/2020_12_24_165238_create_students_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('code', 10);
+            $table->string('name', 50);
+            $table->string('lastname', 50);
+            $table->date('birthday');
+            $table->integer('career_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('career_id')->references('id')->on('careers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
